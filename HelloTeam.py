9ed874c8981a45d5a0b07960af2a22cb5cda5069
Lee Seversky <gitlabgi@seversky.net>
2022-03-05 22:00:59 -0500

    Fixed a few errors that were missed and cleaned up print statements

--- HelloTeam.py
+++ HelloTeam.py
@@ -1,56 +1,19 @@
 #Load and print the team members in different forms
 
-print("Hello Team")
-
-
-#teamMembers = buildTeam()
-teamMembers = buildTeam('teamlist.txt')
-print("The Team")
-print(team_members)
-print("-----")
-
-print("Printing Team Members from Longest Name to Shortest")
-printLongestToShortest(teamMembers)
-print("-----")
-
-
-print("Printing Team Members from Shortest Name to Longest")
-printShortesToLongest(teamMembers)
-print("-----")
-
-
-print("Printing Team Members from A-Z ")
-printAscendingAlphabetical(teamMembers)
-print("-----")
-
-
-print("Printing Team Members from Z-A")
-printDescendingAlphabetical(teamMembers)
-print("-----")
-
-
-
-
 #Complete the following functions below
 
-def buildTeam(filename)
-    myTeam = []
-    with open(filename) as my_file:
-    for line in my_file:
-        myTeam.append(line)
-    return myTeam
-
-
-def buildTeam()
+def buildTeam(filename):
     """
-    Builds an array of team members
+    Builds an array of team members from file
 
     Returns: array of strings
     """
 
-    team_members = []
-    team_members.append('Lee')
-    return team_members
+    myTeam = []
+    with open(filename) as my_file:
+        for line in my_file:
+            myTeam.append(line.strip())
+    return myTeam
 
 
 #Function 1
@@ -73,5 +36,34 @@ def printAscendingAlphabetical(_members):
 #Function 4
 def printDescendingAlphabetical(_members):
     print(_members)      
+
+
+print("Hello Team")
+
+
+#teamMembers = buildTeam()
+teamMembers = buildTeam('teamlist.txt')
+print("The Team")
+print(teamMembers)
+print("-----")
+
+print("Printing Team Members from Longest Name to Shortest")
+printLongestToShortest(teamMembers)
+print("-----")
+
+
+print("Printing Team Members from Shortest Name to Longest")
+printShortesToLongest(teamMembers)
+print("-----")
+
+
+print("Printing Team Members from A-Z ")
+printAscendingAlphabetical(teamMembers)
+print("-----")
+
+
+print("Printing Team Members from Z-A")
+printDescendingAlphabetical(teamMembers)
+print("-----")
 
 
